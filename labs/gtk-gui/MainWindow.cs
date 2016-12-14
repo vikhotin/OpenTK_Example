b563b94976cf4b15d74d6856e6fad53b86cbefbb
vikhotin <vikhotin@yandex.ru>
2016-12-14 14:56:00 +0300

    GUI container added

--- labs/gtk-gui/MainWindow.cs
+++ labs/gtk-gui/MainWindow.cs
@@ -3,6 +3,8 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.HBox hbox1;
+
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
@@ -10,12 +12,17 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.hbox1 = new global::Gtk.HBox();
+		this.hbox1.Name = "hbox1";
+		this.hbox1.Spacing = 6;
+		this.Add(this.hbox1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
 		}
-		this.DefaultWidth = 400;
-		this.DefaultHeight = 300;
+		this.DefaultWidth = 559;
+		this.DefaultHeight = 378;
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
 	}
