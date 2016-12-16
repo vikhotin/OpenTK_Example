a686cbb3108f54cd074576e450ed33440fd1526f
poiufd <s.volk95@mail.ru>
2016-12-16 11:55:47 +0300

    Figure test completed

--- labs/Figure.cs
+++ labs/Figure.cs
@@ -3,6 +3,45 @@ using OpenTK;
 
 namespace labs
 {
+	
+	[Serializable]
+	public class FigureZeroSizeException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:FigureZeroSizeException"/> class
+		/// </summary>
+		public FigureZeroSizeException ()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:FigureZeroSizeException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		public FigureZeroSizeException (string message) : base (message)
+		{
+			
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:FigureZeroSizeException"/> class
+		/// </summary>
+		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+		/// <param name="inner">The exception that is the cause of the current exception. </param>
+		public FigureZeroSizeException (string message, Exception inner) : base (message, inner)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:FigureZeroSizeException"/> class
+		/// </summary>
+		/// <param name="context">The contextual information about the source or destination.</param>
+		/// <param name="info">The object that holds the serialized object data.</param>
+		protected FigureZeroSizeException (System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base (info, context)
+		{
+		}
+	}
+
 	public abstract class Figure
 	{
 		public Vector3[] Vertices
@@ -22,6 +61,11 @@ namespace labs
 	{
 		public Cube(float width)
 		{
+
+			if (width == 0)
+				throw new FigureZeroSizeException("Cube size = 0");
+
+
 			Vertices = new Vector3[8];
 			Vertices[0] = new Vector3(0, 0, 0);
 			Vertices[1] = new Vector3(0, width, 0);
@@ -46,6 +90,9 @@ namespace labs
 	{
 		public Pyramid(float width)
 		{
+			if (width == 0)
+				throw new FigureZeroSizeException("Pyramide size = 0");
+			
 			float r3 = Convert.ToSingle(Math.Sqrt(3));
 			float r6 = Convert.ToSingle(Math.Sqrt(6));
 
