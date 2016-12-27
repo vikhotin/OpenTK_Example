8be25da772265533f1931858506febbcd1d5c00a
poiufd <s.volk95@mail.ru>
2016-12-27 11:32:33 +0300

    Last integration test added

---
+++ labs/tests/test_render.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using OpenTK;
+
+namespace labs
+{
+	[TestFixture ()]
+	public class test_render
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Camera _cam = new Camera (new Vector3 (0, 0, 0), new Vector3 (1, 1, 1));
+			Cube _cube = new Cube (10);
+			Scene _sc = new Scene (_cam, new Figure[1] { _cube });
+
+			Renderer _ren = new Renderer (_sc);
+			Assert.AreEqual (_sc,_ren.scene);
+		}
+	}
+}
+
