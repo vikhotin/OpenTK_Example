02833a7db2a12b892b7e31bf37e4e39ca1fc97dc
poiufd <s.volk95@mail.ru>
2017-01-15 22:55:57 +0300

    for pyramid

--- labs/tests/test_figure.cs
+++ labs/tests/test_figure.cs
@@ -85,6 +85,13 @@ namespace labs
 				new Vector3 (0, width / r3, -width * r6 / 9),
 				new Vector3 (0, 0, (3 * width * r3 - width * r6)/9)
 			}, _pyr.Vertices);
+
+
+			Assert.AreEqual (new int[4][] {
+				new int[3] { 0, 1, 2 },
+				new int[3] { 0, 1, 3 },
+				new int[3] { 0, 2, 3 },
+				new int[3] { 1, 2, 3 }}, _pyr.Faces);
 		}
 	}
 }
